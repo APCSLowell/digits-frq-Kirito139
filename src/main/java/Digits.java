da3f2fb65ecf3c9b0701a6ccadd716032ffa5836
Lennier Mulder <lenniermulder@icloud.com>
2025-05-09 22:14:39 -0700

    First Attempt

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -7,13 +7,23 @@ public class Digits
 
 	public Digits(int num)
 	{ /* to be implemented in part (a) */ 
-	    
+	    while (num / 10 != 0) {
+            digitList.set(0, num % 10);
+            num = num / 10;
+        }
+        digitList.add(num);
 	}
 
 	public boolean isStrictlyIncreasing()
 	{ /* to be implemented in part (b) */
-		
-
+        int i = 0;
+        for (int digit : digitList) {
+            if (digit < digitList.get(i)) {
+                return false;
+            }
+            i++;
+        }
+        return true;
 	}
 	
 	public String toString()
